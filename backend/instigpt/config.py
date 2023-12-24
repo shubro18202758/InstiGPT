2830e148baf92679ae60f7c5af786e2726ae6403
Akshaan Khan <akshaankhan2004@gmail.com>
2023-12-25 03:41:04 +0530

    Modified config

--- backend/instigpt/config.py
+++ backend/instigpt/config.py
@@ -2,8 +2,8 @@
 EMBEDDING_MODEL = "thenlper/gte-large"
 
 # Retriver
-DATA_PATH = "data/ugrulebook.json"
-#use "data/resobin_courses.json"
+DATA_PATH = "data/resobin_courses.json"
+#use "data/resobin_courses.json" or "data/ugrulebook.json"
 COLLECTION_NAME = "resonin_courses"
 #use "ugrulebook" or "resonin_courses"
 
