a95ec6f6f946f242db237348ec34f03c258609f1
Geet Sethi <geetsethi05@gmail.com>
2023-12-26 09:04:49 +0530

    fix: include conversation router in main app

--- backend/instigpt/api/__init__.py
+++ backend/instigpt/api/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from . import auth
+from . import auth, conversation
 
 app = FastAPI()
 
@@ -14,6 +14,7 @@ app.add_middleware(
 )
 
 app.include_router(auth.router)
+app.include_router(conversation.router)
 
 
 @app.get("/")
--- backend/instigpt/api/conversation.py
+++ backend/instigpt/api/conversation.py
@@ -1,9 +1,11 @@
+from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 
 from instigpt.llm import get_embeddings, get_chain, get_generator_model, get_retriever
 
-from . import app
 from .input_models import ChatInput
+
+router = APIRouter()
 
 embeddings = get_embeddings()
 llm = get_generator_model()
@@ -11,11 +13,12 @@ retriever = get_retriever(embeddings=embeddings)
 chain = get_chain(llm=llm, retriever=retriever)
 
 
-@app.post("/chat")
+@router.post("/chat")
 async def chat(input: ChatInput):
     output = chain.stream(
         {
-            "question": input.messages[-1].content + " according to sources of IIT Bombay",
+            "question": input.messages[-1].content
+            + " according to sources of IIT Bombay",
             "chat_history": "\n\n".join(map(str, input.messages[:-1] or [])) or "None",
         }
     )
