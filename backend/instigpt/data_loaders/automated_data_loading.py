8eb6b960e38e8ab11f04f45c34741efcb0d29bda
Akshaan Khan <akshaankhan2004@gmail.com>
2024-01-17 19:03:05 +0530

    Updated DataLoader automated

--- backend/instigpt/data_loaders/automated_data_loading.py
+++ backend/instigpt/data_loaders/automated_data_loading.py
@@ -21,7 +21,7 @@ load_dotenv()
 # Document Paths
 PDFS = ["Apping Guide Booklet", "Bluebook Edition Three", "Course Info Booklet 2020-21", "Non-Core Apping Guide", "SAC-Constitution-March-2018", "MInDS Minor", "itc_report"]
 PDFS = ["pdf/" + pdf + ".pdf" for pdf in PDFS]
-JSONS = ["resobin_courses_final", "ugrulebook", "grades"] # "dept_web_scrap", "misc_web_scrap"
+JSONS = ["resobin_courses_final", "ugrulebook"] # "dept_web_scrap", "misc_web_scrap"
 JSONS = ["json/" + json + ".json" for json in JSONS]
 URLS_DEPT = ["aero", "bio", "che", "chem", "civil", "earth", "eco", "elec", "env", "hss", "maths", "mech"]
 URLS_DEPT = ["urls/dept/" + url + ".json" for url in URLS_DEPT]
