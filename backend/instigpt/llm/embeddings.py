390d406a7daad6d73d6e7428438a2b2460be741b
Geet Sethi <geetsethi05@gmail.com>
2024-01-31 18:10:57 +0530

    Revert "Add Voyage embeddings"
    This reverts commit 33d58750291bd19a0e60fb4dc100a4a6ca74d175.

--- backend/instigpt/llm/embeddings.py
+++ backend/instigpt/llm/embeddings.py
@@ -1,16 +1,12 @@
 from langchain_core.embeddings import Embeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.embeddings import VoyageEmbeddings
 
 # from langchain.embeddings import HuggingFaceEmbeddings
 
 from instigpt import config
 
 
-
-
 def get_embeddings() -> Embeddings:
     # embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDING_MODEL)
-    # embeddings = GoogleGenerativeAIEmbeddings(model=config.EMBEDDING_MODEL)  # type: ignore
-    embeddings = VoyageEmbeddings(voyage_api_key="pa-d0rh4n-08o1-yY5HkGrz8co84wUhZoidq92Ppr00lpw", model="voyage-02")
+    embeddings = GoogleGenerativeAIEmbeddings(model=config.EMBEDDING_MODEL)  # type: ignore
     return embeddings
