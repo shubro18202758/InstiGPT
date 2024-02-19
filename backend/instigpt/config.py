b4f18124eeb8f3b5d1afbc0f0ee477684f1f0f93
Geet Sethi <geetsethi05@gmail.com>
2024-02-19 22:15:06 +0530

    store api key in env var and remove unnecessary deps in backend

--- backend/instigpt/config.py
+++ backend/instigpt/config.py
@@ -1,5 +1,6 @@
 # Embedder
-EMBEDDING_MODEL = "jinaai/jina-embeddings-v2-base-en"
+EMBEDDING_MODEL = "voyage-large-2"
+# EMBEDDING_MODEL = "jinaai/jina-embeddings-v2-base-en"
 # EMBEDDING_MODEL = "models/embedding-001"
 
 # Retriever
@@ -11,4 +12,8 @@ GENERATOR_TEMPERATURE = 0
 
 COOKIE_NAME = "session_id"
 
-FRONTEND_ORIGINS = ["http://localhost:3000", "http://localhost:8080", "https://gymkhana.iitb.ac.in/instigpt"]
+FRONTEND_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "https://gymkhana.iitb.ac.in/instigpt",
+]
--- backend/instigpt/llm/embeddings.py
+++ backend/instigpt/llm/embeddings.py
@@ -1,9 +1,10 @@
 import os
 
 from langchain_core.embeddings import Embeddings
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from huggingface_hub import login
 from langchain_community.embeddings import VoyageEmbeddings
+
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+# from huggingface_hub import login
 
 # from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
@@ -17,7 +18,10 @@ def get_embeddings() -> Embeddings:
 
     # embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDING_MODEL, model_kwargs = {'device': 'cpu'})
     # embeddings = GoogleGenerativeAIEmbeddings(model=config.EMBEDDING_MODEL)  # type: ignore
-    
-    embeddings = VoyageEmbeddings(voyage_api_key="pa-rkmFdeflRlZT3dastzQP5E8J_ee7A82rKwLtyG7I8v8", model="voyage-large-2")
+
+    embeddings = VoyageEmbeddings(
+        voyage_api_key=os.environ["VOYAGE_API_KEY"],  # type: ignore
+        model=config.EMBEDDING_MODEL,
+    )
 
     return embeddings
