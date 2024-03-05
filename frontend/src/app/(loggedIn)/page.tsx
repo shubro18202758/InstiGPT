06e1be01e56996a36ee845a391a84c26bd0f59a2
Geet Sethi <geetsethi05@gmail.com>
2024-03-05 15:41:06 +0530

    add actual examples to home page

--- frontend/src/app/(loggedIn)/page.tsx
+++ frontend/src/app/(loggedIn)/page.tsx
@@ -1,27 +1,34 @@
 import { LogoWithText } from "@/components/Logo";
+import { NewChatButton } from "@/components/navbar/NewChatButton";
 
 export default function Home() {
   return (
     <div className="mx-auto flex max-w-3xl flex-col justify-center px-3 text-center xl:max-w-4xl">
       <div>
-        <div className="mx-10 mb-3 flex items-center text-2xl font-semibold">
-          <LogoWithText size={150} />
+        <div className="mb-3 flex items-center justify-center font-semibold">
+          <LogoWithText className="h-24" />
         </div>
         <p className="text-base text-gray-400">
           Answer to all the questions you have about IIT Bombay.
         </p>
       </div>
+      <div className="mt-6 w-80 max-w-full self-center md:hidden">
+        <NewChatButton />
+      </div>
       <div className="mt-6">
         <p className="mb-3 text-gray-300">Examples</p>
         <div className="grid gap-3 lg:grid-cols-3 lg:gap-5">
-          {["Example 1", "Example 2", "Example 3"].map((example, i) => (
-            <button
+          {[
+            "What are the required credits to complete Honors degree?",
+            "Who is Prof Manjesh K Hanawal?",
+            "Recommend me 3 courses if I want to pursue Biomechanics",
+          ].map((example, i) => (
+            <span
               key={i}
-              type="button"
-              className="rounded-xl border border-gray-800 bg-gray-800 p-2.5 text-gray-300 hover:bg-gray-700 sm:p-4"
+              className="rounded-xl border border-gray-800 bg-gray-800 p-2.5 text-sm text-gray-300 hover:bg-gray-700 sm:p-4 md:text-base"
             >
               {example}
-            </button>
+            </span>
           ))}
         </div>
       </div>
